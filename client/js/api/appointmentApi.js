afb7c09bb0bd36cbf921e5ef7660a4ed55499990
Nawin chander <nawinnapzy289@gmail.com>
2026-05-14 13:30:11 +0530

    add

---
+++ client/js/api/appointmentApi.js
@@ -0,0 +1,30 @@
+const API =
+'http://localhost:5000/api/appointments';
+
+export const getAppointments =
+async () => {
+
+  const res =
+    await fetch(API);
+
+  return res.json();
+};
+
+export const createAppointment =
+async (data) => {
+
+  const res =
+    await fetch(API, {
+
+      method: 'POST',
+
+      headers: {
+        'Content-Type':
+        'application/json'
+      },
+
+      body: JSON.stringify(data)
+    });
+
+  return res.json();
+};

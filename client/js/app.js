b57018601b3561090bd3c92c6d5c420dd134c78b
Nawin chander <nawinnapzy289@gmail.com>
2026-05-14 13:30:43 +0530

    add

---
+++ client/js/app.js
@@ -0,0 +1,90 @@
+import {
+  getAppointments,
+  createAppointment
+}
+from './api/appointmentApi.js';
+
+const patient =
+document.getElementById(
+  'patient'
+);
+
+const doctor =
+document.getElementById(
+  'doctor'
+);
+
+const bookBtn =
+document.getElementById(
+  'bookBtn'
+);
+
+const queueList =
+document.getElementById(
+  'queueList'
+);
+
+const renderAppointment =
+(appointment) => {
+
+  const div =
+    document.createElement('div');
+
+  div.className =
+    'queue-card';
+
+  div.innerHTML = `
+    <h3>
+      ${appointment.patientName}
+    </h3>
+
+    <p>
+      Doctor:
+      ${appointment.doctorName}
+    </p>
+
+    <p>
+      Token:
+      ${appointment.tokenNumber}
+    </p>
+  `;
+
+  queueList.appendChild(div);
+};
+
+const loadAppointments =
+async () => {
+
+  const appointments =
+    await getAppointments();
+
+  appointments.forEach(
+    renderAppointment
+  );
+};
+
+bookBtn
+.addEventListener(
+'click',
+
+async () => {
+
+  const appointment = {
+
+    patientName:
+      patient.value,
+
+    doctorName:
+      doctor.value
+  };
+
+  const saved =
+    await createAppointment(
+      appointment
+    );
+
+  renderAppointment(saved);
+});
+
+loadAppointments();
+

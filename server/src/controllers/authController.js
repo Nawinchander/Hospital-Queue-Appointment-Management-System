27e16ea3748c748c27acff942fab2d8fc52fc495
Nawin chander <nawinnapzy289@gmail.com>
2026-05-16 14:33:24 +0530

    add

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,91 @@
+const bcrypt =
+require('bcryptjs');
+
+const jwt =
+require('jsonwebtoken');
+
+const User =
+require('../models/User');
+
+exports.register =
+async (req, res) => {
+
+  const {
+    name,
+    email,
+    password
+  } = req.body;
+
+  const hashedPassword =
+    await bcrypt.hash(
+      password,
+      10
+    );
+
+  const user =
+    await User.create({
+
+      name,
+      email,
+
+      password:
+      hashedPassword
+    });
+
+  res.status(201)
+  .json(user);
+};
+
+exports.login =
+async (req, res) => {
+
+  const {
+    email,
+    password
+  } = req.body;
+
+  const user =
+    await User.findOne({
+      email
+    });
+
+  if (!user) {
+
+    return res.status(400)
+    .json({
+      message:
+      'User Not Found'
+    });
+  }
+
+  const isMatch =
+    await bcrypt.compare(
+      password,
+      user.password
+    );
+
+  if (!isMatch) {
+
+    return res.status(400)
+    .json({
+      message:
+      'Invalid Password'
+    });
+  }
+
+  const token =
+    jwt.sign(
+
+      {
+        id: user._id
+      },
+
+      'secretkey',
+
+      {
+        expiresIn: '1d'
+      }
+    );
+
+  res.json({ token });
+};

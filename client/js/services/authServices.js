3216d44ae5f972c36bb90f31518602e87265149b
Nawin chander <nawinnapzy289@gmail.com>
2026-05-17 12:24:25 +0530

    add

---
+++ client/js/services/authServices.js
@@ -0,0 +1,16 @@
+export const saveToken =
+(token) => {
+
+  localStorage.setItem(
+    'token',
+    token
+  );
+};
+
+export const getToken =
+() => {
+
+  return localStorage.getItem(
+    'token'
+  );
+};

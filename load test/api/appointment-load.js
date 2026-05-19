1137a8bc04fc07f47f0ad63327c486c66d9881f5
Nawin chander <nawinnapzy289@gmail.com>
2026-05-19 13:31:40 +0530

    add

---
+++ load test/api/appointment-load.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+
+import { sleep } from 'k6';
+
+export const options = {
+
+  vus: 100,
+
+  duration: '30s'
+};
+
+export default function () {
+
+  http.get(
+    'http://localhost/api/appointments'
+  );
+
+  sleep(1);
+}
